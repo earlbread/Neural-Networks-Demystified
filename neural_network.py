a8dbf504dc341133d235a8e95efc695b1542081d
Seunghun Lee <waydi1@gmail.com>
2017-01-06 01:41:53 -0800

    Add neural network class and forward propagation

---
+++ neural_network.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class NeuralNetwork():
+    def __init__(self):
+        # Hyper parameters
+        self.input_layer_size = 2
+        self.output_layer_size = 1
+        self.hidden_layer_size = 3
+
+        # Weights
+        self.W1 = np.random.randn(self.input_layer_size, self.hidden_layer_size)
+        self.W2 = np.random.randn(self.hidden_layer_size, self.output_layer_size)
+
+    def forward(self, X):
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        y_hat = self.sigmoid(self.z3)
+
+        return y_hat
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
