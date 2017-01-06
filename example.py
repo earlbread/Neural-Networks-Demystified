0bcf11ac0e089e9e598e0b80fcd7b3448133011e
Seunghun Lee <waydi1@gmail.com>
2017-01-06 01:42:51 -0800

    Add example to check the result of neural network

---
+++ example.py
@@ -0,0 +1,13 @@
+import numpy as np
+from neural_network import NeuralNetwork
+
+X = np.array([[3, 5], [5, 1], [10, 2]])
+y = np.array([0.75, 0.82, 0.93])
+
+nn = NeuralNetwork()
+
+y_hat = nn.forward(X)
+
+print(X)
+print(y)
+print(y_hat)
